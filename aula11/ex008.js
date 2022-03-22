c8ad62897d319e6b4e5909963e064cf237d88128
Diogo SAntos <diogo.santos@techremotehub.com>
2022-03-22 15:34:41 +0000

    Create ex008.js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`À velocidade do sei carro é ${vel}Km/h`)
+if (vel>50){ //condição simples
+    console.log(`Vocé ultrapassou a velocidade pertendida`)
+}
+console.log('Dirija sempre com sinto de segurança')
